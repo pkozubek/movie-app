c91a315a0ae4dfa08ef97d255ea734d7144d6192
kozi <pkozubek92@gmail.com>
2019-09-29 14:21:57 +0200

    Updates

---
+++ precache-manifest.2080076aa256eb9eb653d54d0da18d8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ca6e92a4b754662c5d92236a5d55f0",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "9b3da238cdaa193958ce",
+    "url": "/movie-app/static/css/main.0d923bbc.chunk.css"
+  },
+  {
+    "revision": "986d9907159589723034",
+    "url": "/movie-app/static/js/2.22f3979d.chunk.js"
+  },
+  {
+    "revision": "9b3da238cdaa193958ce",
+    "url": "/movie-app/static/js/main.7f0fab16.chunk.js"
+  },
+  {
+    "revision": "53d8450b77d0de299820",
+    "url": "/movie-app/static/js/runtime-main.c0c765f9.js"
+  }
+]);
